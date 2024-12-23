b8fd393f3b2902f301657c9dd5c0e70659497e62
FSD7-Boy <boyzasad01@gmail.com>
2024-12-23 11:39:22 +0700

    Algorithm Skill Checkpoint

--- question-3.js
+++ question-3.js
@@ -1,20 +1,23 @@
 /*
 What is the Big O notation for Function findStudentById . Explain your reason.
-Answer:
+Answer: 
+        What is the Big O notation : o(n)  
+        Explain your reason : เพราะเป็นการหาเพื่อตรวจสอบค่าทั้งหมดใน students(Array) จนกว่าจะเจอค่าที่ตรงเงื่อนไข หรือกรณีแย่ที่สุดหากระทั่งครบทุกตัวจึงเป็นการทำงานของ  o(n) ซึ่งจะตรงกับการใช้ Linear search algorithm สำหรับข้อมูลที่ไม่มีการเรียงลำดับ
 
 
 */
 
 /* 
 What is the Big O notation for Function findProductPrice. Explain your reason.
-Answer:
-
+Answer: 
+        What is the Big O notation : o(log n) 
+        Explain your reason : เป็นการหาค่าแบบแบ่งครึ่งในแต่ละการค้นหาของแต่ละรอบจนกว่าจะเจอค่าที่ต้องการ หรือกรณีแย่ที่สุดหากระทั่งครบทุกตัวจึงเป็นการทำงานของ o(log n)  ซึ่งจะตรงกับการใข้งาน Binary search algorithm สำหรับข้อมูลที่มีการเรียงลำดับแล้ว
 
 */
 
 /* 
 Which function is more efficient and why?
-Answer:
+Answer: ถ้าในกรณีที่มีจำนวนข้อมูลที่มีมหาศาลการใช้ o(log n) :  Binary search algorithm จะดีกว่าเนื่องจากจะทำงานได้ไวมากกว่าในการค้นหาข้อมูลในแบบของ o(n) : Linear search algorithm เพราะ เป็นการหาค่าเพื่อตรวจสอบข้อมูลทั้งหมดตั้งแต่เริ่มต้นเรียงไปจนกว่าทั้งพบเงื่อนไขหรือค่าที่ต้องการ ซึ่งหากข้อมูลมีจำนวนมากและค่าของผลลัพธ์อยู่ลำดับสุดท้ายอาจทำให้การค้นหาเสียเวลามากกว่าการใช้ในรูปแบบของ  o(log n) :  Binary search algorithm
 
 
 */
--- question-5.js
+++ question-5.js
@@ -1,6 +1,7 @@
 /*  
 What is the Big O notation for Function sortProductsByPrice of the Question 4. Explain your reason.
 Answer:
-
+            What is the Big O notation : o(n^2)
+            Explain your reason : การใช้ Bubble sort algorithm ตรงกับเงื่อนไขของ o(n^2) เนื่องจากต้องการสลับข้อมูลข้างในของ array ทั้งหมดและมีการเรียงลำดับจากน้อยไปมากโดยการกำหนด products[j].price > products[j + 1].price ซึ่งเหมาะสำหรับการใช้ในกรณีการเรียงลำดับข้อมูล
 
 */
